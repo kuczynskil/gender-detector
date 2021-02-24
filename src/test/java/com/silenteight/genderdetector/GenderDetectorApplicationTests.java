7a08d51ec151adf3bd3aa6c2c4efb92dbc787f33
kuczynskil <l.kuczynski95@gmail.com>
2021-02-24 22:07:57 +0100

    Create project's internal structure

---
+++ src/test/java/com/silenteight/genderdetector/GenderDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silenteight.genderdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
