824949f94e5e43e410464222aac25076ff7583f5
kuczynskil <l.kuczynski95@gmail.com>
2021-02-25 13:15:35 +0100

    Add Service class and it's test class

---
+++ src/main/java/com/silenteight/genderdetector/algorithm/Service_v1.java
@@ -0,0 +1,14 @@
+package com.silenteight.genderdetector.algorithm;
+
+public class Service_v1 implements Service {
+
+    @Override
+    public boolean detectGenderByFirstTokenOfName() {
+        return false;
+    }
+
+    @Override
+    public boolean detectGenderByMajorityRule() {
+        return false;
+    }
+}
---
+++ src/test/java/com/silenteight/genderdetector/algorithm/Service_v1Test.java
@@ -0,0 +1,13 @@
+package com.silenteight.genderdetector.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Service_v1Test {
+
+    @Test
+    void name() {
+
+    }
+}
