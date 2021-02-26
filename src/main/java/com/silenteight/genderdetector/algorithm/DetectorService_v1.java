4b6a777652353990fa8c14f6621ae8aa8dcd0195
kuczynskil <l.kuczynski95@gmail.com>
2021-02-26 12:04:24 +0100

    Add business logic to gender detecting algorithm
    This commit also contains added tests for
    DetectorService_v1 class.

--- src/main/java/com/silenteight/genderdetector/algorithm/DetectorService_v1.java
+++ src/main/java/com/silenteight/genderdetector/algorithm/DetectorService_v1.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 public class DetectorService_v1 implements DetectorService {
 
     private final TokenReader reader;
+    private static final String FEMALE = "FEMALE";
+    private static final String MALE = "MALE";
+    private static final String INCONCLUSIVE = "INCONCLUSIVE";
 
     public DetectorService_v1(TokenReader reader) {
         this.reader = reader;
@@ -13,12 +16,33 @@ public class DetectorService_v1 implements DetectorService {
 
     @Override
     public String detectGenderByFirstTokenOfName(String name) {
-        return "FEMALE";
+        if (name.length() == 0 || name.length() > 100) return INCONCLUSIVE;
+
+        name = name.trim();
+        name = name.split(" ")[0];
+        return getGender(name);
     }
 
     @Override
     public String detectGenderByMajorityRule(String name) {
-        return "FEMALE";
+        if (name.length() == 0 || name.length() > 100) return INCONCLUSIVE;
+
+        name = name.trim();
+        String[] names = name.split(" ");
+        int maleTokens = 0;
+        int femaleTokens = 0;
+
+        for (String s : names) {
+            if (getGender(s).equals(MALE)) maleTokens++;
+            else if (getGender(s).equals(FEMALE)) femaleTokens++;
+        }
+        if (maleTokens == femaleTokens) return INCONCLUSIVE;
+        return maleTokens > femaleTokens ? MALE : FEMALE;
     }
 
+    private String getGender(String name) {
+        if (reader.isInFemaleTokenList(name)) return FEMALE;
+        else if (reader.isInMaleTokenList(name)) return MALE;
+        else return INCONCLUSIVE;
+    }
 }
--- src/test/java/com/silenteight/genderdetector/algorithm/DetectorService_v1Test.java
+++ src/test/java/com/silenteight/genderdetector/algorithm/DetectorService_v1Test.java
@@ -8,22 +8,79 @@ class DetectorService_v1Test {
 
     private final TokenReader reader = new TokenReader();
     private final DetectorService_v1 detector = new DetectorService_v1(reader);
+    private final String FEMALE = "FEMALE";
+    private final String MALE = "MALE";
+    private final String INCONCLUSIVE = "INCONCLUSIVE";
 
     @Test
     void returns_FEMALE_if_first_token_of_name_is_in_female_token_list() {
-        String female = "FEMALE";
 
-        assertEquals(female, detector.detectGenderByFirstTokenOfName("Maria "));
+        assertEquals(FEMALE, detector.detectGenderByFirstTokenOfName("Maria"));
+        assertEquals(FEMALE, detector.detectGenderByFirstTokenOfName("Maria Monika"));
+        assertEquals(FEMALE, detector.detectGenderByFirstTokenOfName("Gabriela"));
+
+        assertNotEquals(FEMALE, detector.detectGenderByFirstTokenOfName("Jan Maria Rokita"));
+        assertNotEquals(FEMALE, detector.detectGenderByFirstTokenOfName("Andrzej Agnieszka Agata"));
+        assertNotEquals(FEMALE, detector.detectGenderByFirstTokenOfName("Bogdan    1223"));
     }
 
     @Test
     void returns_MALE_if_first_token_of_name_is_in_male_token_list() {
-        String male = "MALE";
 
-        assertEquals(male, detector.detectGenderByFirstTokenOfName("Jan"));
+        assertEquals(MALE, detector.detectGenderByFirstTokenOfName("Jan"));
+        assertEquals(MALE, detector.detectGenderByFirstTokenOfName("Jan Maria Rokita"));
+        assertEquals(MALE, detector.detectGenderByFirstTokenOfName("wOjciech"));
+
+        assertNotEquals(MALE, detector.detectGenderByFirstTokenOfName("Anna"));
+        assertNotEquals(MALE, detector.detectGenderByFirstTokenOfName("Anna Jan Marian"));
+        assertNotEquals(MALE, detector.detectGenderByFirstTokenOfName("Wiktoria  admin1234"));
     }
 
     @Test
-    void detectGenderByMajorityRule() {
+    void returns_FEMALE_if_majority_of_names_are_in_female_token_list() {
+
+        assertEquals(FEMALE, detector.detectGenderByMajorityRule("Maria"));
+        assertEquals(FEMALE, detector.detectGenderByMajorityRule("Maria Monika"));
+        assertEquals(FEMALE, detector.detectGenderByMajorityRule("Maria Monika Wojciech"));
+        assertEquals(FEMALE, detector.detectGenderByMajorityRule("Anna Katarzyna Aleksandra Wojciech Adam"));
+
+        assertNotEquals(FEMALE, detector.detectGenderByMajorityRule("Maria Maria Jan Jan Jan"));
+        assertNotEquals(FEMALE, detector.detectGenderByMajorityRule("Jan Anna Wojciech"));
+        assertNotEquals(FEMALE, detector.detectGenderByMajorityRule("Admin User Krzysztof Jaroslaw"));
+    }
+
+    @Test
+    void returns_MALE_if_majority_of_names_are_in_male_token_list() {
+
+        assertEquals(MALE, detector.detectGenderByMajorityRule("Jan Marek"));
+        assertEquals(MALE, detector.detectGenderByMajorityRule("Marcin"));
+        assertEquals(MALE, detector.detectGenderByMajorityRule("Marcin Łukasz Anna Zuzanna Michał"));
+        assertEquals(MALE, detector.detectGenderByMajorityRule("Gertruda Zbigniew Włodzimierz"));
+
+        assertNotEquals(MALE, detector.detectGenderByMajorityRule("Maria"));
+        assertNotEquals(MALE, detector.detectGenderByMajorityRule("Maria Monika"));
+        assertNotEquals(MALE, detector.detectGenderByMajorityRule("    Maria Monika Wojciech   "));
+    }
+
+    @Test
+    void returns_INCONCLUSIVE_if_the_input_String_contains_the_same_number_of_female_and_male_names() {
+
+        assertEquals(INCONCLUSIVE, detector.detectGenderByMajorityRule("Jan Anna"));
+        assertEquals(INCONCLUSIVE, detector.detectGenderByMajorityRule("aaa bbb"));
+        assertEquals(INCONCLUSIVE, detector.detectGenderByMajorityRule("admin user"));
+        assertEquals(INCONCLUSIVE, detector.detectGenderByMajorityRule("Marcin xyz Monika    "));
+        assertEquals(INCONCLUSIVE, detector.detectGenderByMajorityRule("   Adam Andrzej Anna Maria   "));
+    }
+
+    @Test
+    void returns_INCONCLUSIVE_if_passed_String_is_empty_or_is_longer_than_100_characters() {
+
+        assertEquals(INCONCLUSIVE, detector.detectGenderByFirstTokenOfName(""));
+        assertEquals(INCONCLUSIVE, detector.detectGenderByFirstTokenOfName("Piotr Monika Grzegorz some other words, " +
+                "where sum of the characters(including whitespaces) is bigger than 100"));
+
+        assertEquals(INCONCLUSIVE, detector.detectGenderByMajorityRule(""));
+        assertEquals(INCONCLUSIVE, detector.detectGenderByMajorityRule("Piotr Monika Grzegorz some other words, " +
+                "where sum of the characters(including whitespaces) is bigger than 100"));
     }
 }
